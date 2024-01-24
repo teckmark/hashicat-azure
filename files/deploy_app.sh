aeab6aff6dcad7cb2bf4a73258900da4dbdf4c98
teckmark <49568404+teckmark@users.noreply.github.com>
2024-01-24 11:37:05 -0800

    Update deploy_app.sh

--- files/deploy_app.sh
+++ files/deploy_app.sh
@@ -13,8 +13,8 @@ cat << EOM > /var/www/html/index.html
 
   <!-- BEGIN -->
   <center><img src="http://${PLACEHOLDER}/${WIDTH}/${HEIGHT}"></img></center>
-  <center><h2>Meow World!</h2></center>
-  Welcome to ${PREFIX}'s app. Replace this text with your own. 
+  <center><h2>"Mark's World!"</h2></center>
+  "Welcome to ${PREFIX}'s app. Would you like to know more?."
   <!-- END -->
   
   </div>
